40bce9d69c583e3e365498751a13ed32c88582e4
Jacob Aluvanze <77957856+aluvanze@users.noreply.github.com>
2022-10-19 16:42:50 +0300

    Add files via upload

--- exam2.py
+++ exam2.py
@@ -1,15 +1,18 @@
 
 n=int(input("enter  number of times\n"))
+
+
 for i in range(n):
  while n!=0:
 
-    num = int(input("enter a number\n"))
-    if num<0:
+    num1= int(input("Enter First Number\n"))
+    num2 = int(input("Enter Second number\n"))
+    if num1<0 or num2<0:
         print("*"*20)
         print(" *enter a positive number*")
         print("*" * 20)
-        continue
-    product=num*num
+        break
+    product=num1*num2
     print("the product:",product)
     break
 
